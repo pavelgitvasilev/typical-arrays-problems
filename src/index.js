8cdc8c75a13b75e485141452d511b1ca31ca7610
PavelVasilev <vpnprint@gmail.com>
2020-10-02 21:12:47 +0300

    feat: finished Typical Arrays Problems test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min;
+  if (Array.isArray(array) && array != '') {
+      min = Math.min.apply(null, array)
+
+  } else {
+    return 0;
+  }
+  return min
+
 }
 
+
 exports.max = function max (array) {
-  return 0;
+  let max;
+  if (Array.isArray(array) && array != '') {
+    max = Math.max.apply(null, array)
+
+  } else {
+    return 0;
+  }
+  return max
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  if (Array.isArray(array) && array != '') {
+    
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i]
+    }
+
+  } else {
+    return 0;
+  }
+  return sum / (array.length)
+
 }
+
